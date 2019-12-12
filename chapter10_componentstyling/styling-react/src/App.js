35927d74e1832f21c54289d4e51916e68c143cb2
oggong <49095304+oggong@users.noreply.github.com>
2019-12-12 14:31:47 +0900

    chapter10_componentstyling updates

---
+++ chapter10_componentstyling/styling-react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import CSSModule from './CSSModule';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <CSSModule />
+      </div>
+    )
+  }
+}
+
+
+export default App;
