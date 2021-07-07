d23e151b5a27f155bfab7910e2313d23a9dd9475
Oh Min Seok <oggong93@naver.com>
2021-07-07 17:42:33 +0900

    resume update

--- React-Portfolio/src/resumeData.js
+++ React-Portfolio/src/resumeData.js
@@ -1,118 +1,132 @@
 let resumeData = {
-    "imagebaseurl":"https://oggong.github.io/",
-    "name": "Minseok Oh",
-    "role": "Frontend Developer and Android Developer",
-    "linkedinId":"omsbrd@naver.com",
-    "skypeid": "Your skypeid",
-    "roleDescription": "",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/민석-오-31908314b",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/minseokoh-dev",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"facebook",
-          "url":"https://web.facebook.com/profile.php?id=100047399688737",
-          "className":"fa fa-facebook"
-        }
-      ],
-    "aboutme":"JavaScript를 활용한 Full Stack 개발자를 지향하고 있으며, React , Android 개발자로의 취직을 희망하고 있습니다.",
-    "address":"omsbrd@naver.com",
-    "website":"https://github.com/minseokoh-dev",
-    "education":[
-      {
-        "UniversityName":"Gyeonggi University Of Science And Technology",
-        "specialization":"컴퓨터 모바일 융합과",
-        "MonthOfPassing":"Fab",
-        "YearOfPassing":"2019",
-        "Achievements":""
-      },
-      {
-        "UniversityName":"Encore Playdata",
-        "specialization":"자바 및 파이썬 기반 빅데이터 시각화 전문가 과정",
-        "MonthOfPassing":"Sep",
-        "YearOfPassing":"2019",
-        "Achievements":""
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"DeltaBot",
-        "specialization":"개발팀 인턴",
-        "MonthOfLeaving":"Fab",
-        "YearOfLeaving":"2019",
-        "Achievements":""
-      },
-      // {
-      //   "CompanyName":"Some Company",
-      //   "specialization":"Some specialization",
-      //   "MonthOfLeaving":"Jan",
-      //   "YearOfLeaving":"2018",
-      //   "Achievements":"Some Achievements"
-      // }
-    ],
-    "skillsDescription":"",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      },
-      {
-        "skillname":"ANDROID"
-      },
-      {
-        "skillname":"JAVA"
-      },
-      {
-        "skillname":"NODEJS"
-      },
-      
-    ],
-    "portfolio":[
-      {
-        "name":"Project-keepfit",
-        "description":"스프링 기반 하이브리드 웹앱 프로젝트",
-        "imgurl":"images/portfolio/keepfit-project.jpg",
-        "portfoliourl":"https://github.com/minseokoh-dev/project-keepfit"
-      },
-      {
-        "name":"Project-balloon",
-        "description":"자바스크립트 기반 Full Stack 웹 프로젝트",
-        "imgurl":"images/portfolio/balloon-project.jpg",
-        "portfoliourl":"https://github.com/minseokoh-dev/project-balloon"
-      },
-      {
-        "name":"blog-project",
-        "description":"리액트 블로그 프로젝트",  
-        "imgurl":"images/portfolio/blog-project.jpg"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/fourth-project.jpg"
-      }
-    ],
-    // "testimonials":[
-    //   {
-    //     "description":"This is a sample testimonial",
-    //     "name":"Some technical guy"
-    //   },
-    //   {
-    //     "description":"This is a sample testimonial",
-    //     "name":"Some technical guy"
-    //   }
-    // ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://oggong.github.io/",
+  name: "Minseok Oh",
+  role: "WEB, APP FRONTEND DEVELOPER",
+  linkedinId: "omsbrd@naver.com",
+  skypeid: "Your skypeid",
+  roleDescription: "",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/민석-오-31908314b",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/minseokoh-dev",
+      className: "fa fa-github",
+    },
+    {
+      name: "facebook",
+      url: "https://web.facebook.com/profile.php?id=100047399688737",
+      className: "fa fa-facebook",
+    },
+  ],
+  aboutme:
+    "JavaScript를 활용한 Full Stack 개발자를 지향하고 있으며, REACTJS, VUEJS, REACTNATIVE 로 WEB, APP 개발자 입니다.",
+  address: "omsbrd@naver.com",
+  website: "https://github.com/minseokoh-dev",
+  education: [
+    {
+      UniversityName: "Gyeonggi University Of Science And Technology",
+      specialization: "컴퓨터 모바일 융합과",
+      MonthOfPassing: "Fab",
+      YearOfPassing: "2019",
+      Achievements: "",
+    },
+    {
+      UniversityName: "Encore Playdata",
+      specialization: "자바 및 파이썬 기반 빅데이터 시각화 전문가 과정",
+      MonthOfPassing: "Sep",
+      YearOfPassing: "2019",
+      Achievements: "",
+    },
+    {
+      UniversityName: "KG ITBANK",
+      specialization: "학점인증제 인정 강의 수료 (18학점)",
+      MonthOfPassing: "Dec",
+      YearOfPassing: "2020",
+      Achievements: "",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "DeltaBot",
+      specialization: "개발팀 인턴",
+      MonthOfLeaving: "Feb",
+      YearOfLeaving: "2019",
+      Achievements: "",
+    },
+    {
+      CompanyName: "AINOMIS",
+      specialization: "개발팀 사원",
+      MonthOfLeaving: "Apr",
+      YearOfLeaving: "2021",
+      Achievements: "",
+    },
+    // {
+    //   "CompanyName":"Some Company",
+    //   "specialization":"Some specialization",
+    //   "MonthOfLeaving":"Jan",
+    //   "YearOfLeaving":"2018",
+    //   "Achievements":"Some Achievements"
+    // }
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "VUEJS",
+    },
+    {
+      skillname: "REACTJS",
+    },
+    {
+      skillname: "JAVASCRIPT",
+    },
+    {
+      skillname: "NODEJS",
+    },
+    {
+      skillname: "REACTNATIVE",
+    },
+    {
+      skillname: "JAVA",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Project-keepfit",
+      description: "스프링 기반 하이브리드 웹앱 프로젝트",
+      imgurl: "images/portfolio/keepfit-project.jpg",
+      portfoliourl: "https://github.com/minseokoh-dev/project-keepfit",
+    },
+    {
+      name: "Project-balloon",
+      description: "자바스크립트 기반 Full Stack 웹 프로젝트",
+      imgurl: "images/portfolio/balloon-project.jpg",
+      portfoliourl: "https://github.com/minseokoh-dev/project-balloon",
+    },
+    {
+      name: "blog-project",
+      description: "리액트 블로그 프로젝트",
+      imgurl: "images/portfolio/blog-project.jpg",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/fourth-project.jpg",
+    },
+  ],
+  // "testimonials":[
+  //   {
+  //     "description":"This is a sample testimonial",
+  //     "name":"Some technical guy"
+  //   },
+  //   {
+  //     "description":"This is a sample testimonial",
+  //     "name":"Some technical guy"
+  //   }
+  // ]
+};
+
+export default resumeData;
